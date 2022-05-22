cab0ee8d84e9534cdfdfbc50ac20f58435c795e3
utyfjs <belarus1990@gmail.com>
2022-05-22 20:37:34 +0400

    feat:done task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const { readdir, mkdir, rm, copyFile } = require('fs/promises');
+const path = require('path');
+
+(async function copyDir() {
+  const dirPath = path.join(__dirname, 'files');
+  await mkdir( dirPath +'-copy',{recursive: true});
+  const copyDirFiles = await readdir(dirPath +'-copy', {withFileTypes: true});
+  for(let file of copyDirFiles){
+    await rm(`${dirPath}-copy\\${file.name}`);
+  }
+  const dirFiles = await readdir(dirPath, {withFileTypes: true});
+  for( let file of dirFiles){
+    if(file.isFile()){
+      console.log(`${dirPath}\\${file.name}`);
+      console.log(`${dirPath}-copy\\${file.name}`);
+      await copyFile(`${dirPath}\\${file.name}`, `${dirPath}-copy\\${file.name}`);
+    }
+  }
+})();
 
